b8540e957bc0a76d63623de6ffec5249794c1fe1
Ryan Duddy <duddy.guy@gmail.com>
2018-01-21 19:42:12 -0800

    Remove unnecesary import

--- staffaccounts.py
+++ staffaccounts.py
@@ -7,7 +7,6 @@
 # Use at your own risk, no liability assumed. Released under the MIT license.
 # for more info see https://github.com/rmduddy/staffaccounts
 
-import datetime
 import config as cfg
 
 class Employee:
